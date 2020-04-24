b5d0f7eaf76a0c2bb0c47f8c8cbaa11e686bd745
wayneeg <waynee@outlook.com.br>
2020-04-23 23:00:15 -0300

    Revert "sdm660-common: Decommonize fstab"
    This reverts commit aaa7b4f47d3bfd4535341c6df57dcc6475aa2926.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -108,6 +108,18 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+ifeq ($(AB_OTA_UPDATER), true)
+LOCAL_SRC_FILES    := etc/fstab_AB.qcom
+else
+LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.goodix.sh
 >>>>>>> parent of aaa7b4f... sdm660-common: Decommonize fstab
 LOCAL_MODULE_TAGS  := optional
